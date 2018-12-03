6da4c37ddba37912f19762ad1c2ce2346bff2ea6
Dean-Chu <zhudi_bupt@163.com>
2018-12-03 22:41:17 +0800

    correct the run scrupt

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
-mv out/image/rootfs.tar.gz ../rootfs_build/rootfs-first.tar.gz
+mv output/image/rootfs.tar.gz ../rootfs_build/rootfs-first.tar.gz
